69affca0cb08961e8cbd32177e418ce6a87839ea
vdps <vdps007@gmail.com>
2023-02-20 14:34:45 +0530

    updated frontend

--- frontend.sh
+++ frontend.sh
@@ -1,7 +1,39 @@
-yum install nginx -y
-rm -rf /usr/share/nginx/html/* 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
-cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip
-systemctl enable nginx
-systemctl start nginx 
+code_dir=$(pwd)
+log_file=/tem/roboshop.log
+rm -f ${log_file}
+
+print_head() {
+    echo -e "\e[36m$1\e[0m]"
+}
+
+ststus_check(){
+    if [$1 -eq 0]; then
+       echo SUCCESS
+    else
+        echo FAILURE
+        echo "Read the log file ${log_file} for more information about error"
+        exit 1
+    fi
+}
+
+print_head "installing nginx"
+yum install nginx -y &>>${log_file}
+status_check $?
+print_head "Removing files"
+rm -rf /usr/share/nginx/html/* &>>${log_file}
+status_check $?
+print_head "Download files"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
+status_check $?
+print_head "copy files"
+cd /usr/share/nginx/html &>>${log_file}
+status_check $?
+print_head "unzip files"
+unzip /tmp/frontend.zip &>>${log_file}
+status_check $?
+print_head "enabeling nginx"
+systemctl enable nginx &>>${log_file}
+status_check $?
+print_head "Starting nginx"
+systemctl restart nginx &>>${log_file}
+status_check $?
