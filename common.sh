4e37f55188057c2027407316605a68d4ced24d17
vdps <vdps007@gmail.com>
2023-02-21 21:11:28 +0530

    dispatch automate

--- common.sh
+++ common.sh
@@ -137,3 +137,20 @@ python() {
 
   systemd_setup
 }
+
+golang() {
+
+  print_head "Install Golang"
+  yum install golang -y &>>${log_file}
+  status_check $?
+
+  app_prereq_setup
+
+  print_head "Download Dependencies"
+  go mod init dispatch &>>${log_file}
+  go get &>>${log_file}
+  go build &>>${log_file}
+  status_check $?
+
+  systemd_setup
+}
