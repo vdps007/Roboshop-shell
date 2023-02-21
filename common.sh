32ca56813fb2e6d0e4e4d81ac23c30bf37f00726
vdps <vdps007@gmail.com>
2023-02-21 14:34:03 +0530

    catalog automate

--- common.sh
+++ common.sh
@@ -16,36 +16,48 @@ status_check(){
     fi
 }
 
-nodejs() {
-    print_head "Download prereq"
-    sudo curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
-    status_check $?
-
-    print_head "Installing nodejs"
-    yum install nodejs -y &>>${log_file}
-    status_check $?
-
+app_prereq_setup(){
     print_head "Adding User"
-    useradd roboshop &>>${log_file}
+    id roboshop &>>${log_file}
+    if [ $? -ne 0 ]; then
+        useradd roboshop &>>${log_file}
+    fi
     status_check $?
 
     print_head "Creating a Dir"
-    mkdir /app &>>${log_file}
+    if [ ! -d /app ]; then
+        mkdir /app &>>${log_file}
+    fi
+    status_check $?
+
+    print_head "Delete Old Content"
+    rm -rf /app/* &>>${log_file}
     status_check $?
 
     print_head "Download and Unzip pakages"
-    curl -L -o /tmp/$(component).zip https://roboshop-artifacts.s3.amazonaws.com/$(component).zip &>>${log_file}
-    cd /app &>>${log_file}
-    unzip /tmp/$(component).zip &>>${log_file}
+    curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
+    cd /app
+    unzip /tmp/${component}.zip &>>${log_file}
+    status_check $?
+}
+
+schema_setup(){
+    print_head "copying repos"
+    cp ${code_dir}/Configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
     status_check $?
 
-    print_head "Installing packages"
-    cd /app &>>${log_file}
-    npm install &>>${log_file}
+    print_head "install mongo-shell"
+    yum install mongodb-org-shell -y &>>${log_file}
     status_check $?
 
+    print_head "Loading mongodb Schema"
+    mongo --host mongodb.itsmevdps.online </app/schema/${component}.js &>>${log_file}
+    status_check $?
+}
+
+systemd_setup(){
     print_head "Copying files"
-    cp ${code_dir}/configs/$(component).service /etc/systemd/system/$(component).service &>>${log_file}
+    cp ${code_dir}/Configs/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
     status_check $?
 
     print_head "Restarting Deamon"
@@ -53,22 +65,31 @@ nodejs() {
     status_check $?
 
     print_head "Enabeling Catalogue"
-    systemctl enable $(component) &>>${log_file}
+    systemctl enable ${component} &>>${log_file}
     status_check $?
 
-    print_head "starting $(component)"
-    systemctl start $(component) &>>${log_file}
+    print_head "starting ${component}"
+    systemctl start ${component} &>>${log_file}
+    status_check $?
+}
+
+nodejs() {
+    print_head "Download prereq"
+    curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
     status_check $?
 
-    print_head "copying repos"
-    cp ${code_dir}/configs/mongodb.repo /etc/systemd/system/mongodb.repo &>>${log_file}
+    print_head "Installing nodejs"
+    yum install nodejs -y &>>${log_file}
     status_check $?
 
-    print_head "install mongo-shell"
-    yum install mongodb-org-shell -y &>>${log_file}
+    app_prereq_setup
+
+    print_head "Installing NodeJS packages"
+    npm install &>>${log_file}
     status_check $?
 
-    print_head "accessing mongodb"
-    mongo --host mongodb.itsmevdps.online </app/schema/$(component).js &>>${log_file}
-    status_check $?
+    schema_setup
+
+    systemd_setup
+    
 }
