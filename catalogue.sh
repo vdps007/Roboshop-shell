668489ca02dde1a94612597785f7d56439e1e89d
vdps <vdps007@gmail.com>
2023-02-21 12:53:18 +0530

    user id if condetion

--- catalogue.sh
+++ catalogue.sh
@@ -12,7 +12,10 @@ yum install nodejs -y &>>${log_file}
 status_check $?
 
 print_head "Adding User"
-useradd roboshop &>>${log_file}
+id roboshop &>>${log_file}
+if [ $? -ne 0 ]; then
+    useradd roboshop &>>${log_file}
+fi
 status_check $?
 
 print_head "Creating a Dir"
