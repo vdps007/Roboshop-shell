1afba2b58f9df59f441be54ffcbaf55c8be631af
vdps <vdps007@gmail.com>
2023-02-21 14:04:55 +0530

    mongo update

--- mongodb.sh
+++ mongodb.sh
@@ -9,7 +9,7 @@ yum install mongodb-org -y &>>${log_file}
 status_check $?
 
 print_head "Changing address"
-sed -e -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${log_file}
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${log_file}
 status_check $?
 
 print_head "Enabling MongoDB"
